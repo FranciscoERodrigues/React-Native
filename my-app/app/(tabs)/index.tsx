cb62658853f2808321e255f3a9d05819a17187ba
Francisco Emerson Rogerio Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2024-11-26 20:31:54 -0300

    Add files via upload

---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function HomeScreen(){
+  return (
+
+    
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',     
+      }}>
+     <Text 
+          style={{
+          color:'white',
+          fontSize: 100,
+      }}> SHOW 
+      </Text>
+
+    </View>
+  );
+};
